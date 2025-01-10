ff581e5c03266d7b7de095f794567d7da4c8a4e7
JIgorSilva <jigorsilva@hotmail.com>
2025-01-10 20:46:23 -0300

    feat(criando tabela produto-component) (#1)

---
+++ src/app/my-components/produto/produto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produto',
+  templateUrl: './produto.component.html',
+  styleUrls: ['./produto.component.css'], // Certifique-se de que o arquivo de estilo existe
+  standalone: true,
+})
+export class ProdutoComponent {
+   
+}
