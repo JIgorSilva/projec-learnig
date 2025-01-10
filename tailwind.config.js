ffc2ec5df4be328692ad0f14d6e8670228d95b0f
JIgorSilva <jigorsilva@hotmail.com>
2025-01-10 11:00:53 -0300

    feat(configurando tailwind lib)

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Garante que o Tailwind seja aplicado aos arquivos HTML e TypeScript
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
