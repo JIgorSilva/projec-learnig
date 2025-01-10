cb2c7ce870d8abb04c33381f0ec2e1dc8fb1bf76
JIgorSilva <jigorsilva@hotmail.com>
2025-01-10 11:32:49 -0300

    fix(ajuste node modulos, e componet) (#2)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProdutoComponent } from './my-components/produto/produto.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html', // Aponta para o template correto
+  styleUrls: ['./app.component.css'], // Aponta para o estilo correto
+  standalone: true,
+  imports: [ProdutoComponent], // Importa ProdutoComponent para uso
 })
 export class AppComponent {
-  title = 'projec-learnig';
+  title = 'Meu Projeto Angular';
 }
